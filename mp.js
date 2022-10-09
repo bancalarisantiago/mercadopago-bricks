1892e87aacdc0ffcec90ad0f6a1221dd656e9cb3
bancalarisantiago <bancalarisantiago@gmail.com>
2022-10-09 19:23:04 -0300

    setup/mp

---
+++ mp.js
@@ -0,0 +1,50 @@
+console.log("test")
+
+const mp = new MercadoPago('TEST-246e7b18-7353-4de7-b81a-d37a68af980e');
+
+const bricksBuilder = mp.bricks();
+
+
+const renderCardPaymentBrick = async (bricksBuilder) => {
+
+const settings = {
+  initialization: {
+    amount: 100, // monto a ser pago
+  },
+  callbacks: {
+    onReady: () => {
+      // callback llamado cuando Brick esté listo
+    },
+    onSubmit: (cardFormData) => {
+      // callback llamado cuando el usuario haga clic en el botón enviar los datos
+
+      // ejemplo de envío de los datos recolectados por el Brick a su servidor
+      return new Promise((resolve, reject) => {
+          fetch("/process_payment", { 
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json",
+              },
+              body: JSON.stringify(cardFormData)
+          })
+          .then((response) => {
+              // recibir el resultado del pago
+              resolve();
+          })
+          .catch((error) => {
+              // tratar respuesta de error al intentar crear el pago
+              reject();
+          })
+        });
+    },
+    onError: (error) => { 
+      // callback llamado para todos los casos de error de Brick
+    },
+  },
+};
+const cardPaymentBrickController = await bricksBuilder.create('cardPayment', 'cardPaymentBrick_container', settings);
+};
+renderCardPaymentBrick(bricksBuilder);
+
+
+
