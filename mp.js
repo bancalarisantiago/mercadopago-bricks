c857121a0f0f191b5d79b3eb6ba7f742e4957ec4
bancalarisantiago <bancalarisantiago@gmail.com>
2022-10-09 20:10:00 -0300

    Back-end route

--- mp.js
+++ mp.js
@@ -20,7 +20,7 @@ const settings = {
 
       // ejemplo de envío de los datos recolectados por el Brick a su servidor
       return new Promise((resolve, reject) => {
-          fetch("/process_payment", { 
+          fetch("http://localhost:3001/api/v1/process_payment", { 
               method: "POST",
               headers: {
                   "Content-Type": "application/json",
